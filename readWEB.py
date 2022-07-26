3f02cb4dfc29158bd2408ac800765421f9b8160e
ElKevin <elkevin@kali.www.kaliorg.com>
2022-07-26 18:17:30 -0400

    Trying to edit Product Data

--- readWEB.py
+++ readWEB.py
@@ -1,10 +1,14 @@
+import click
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 from credentials import CREDENTIALS
 from readSheet import getNewValues
+from xpath import USERXPATH, PASSXPATH, BUTTONXPATH, LISTXPATH, EDIT_PRODUCTFULLXPATH
 
+
+ 
 product_list=['6928475','6841341','6840939']
 
 
@@ -21,6 +25,8 @@ def editProductByIdx(i_prod,Values):
     PRODUCTNAMEXPATH='//*[@name="p_nombre"]'
     PRODUCTPRICEXPATH='//*[@name="p_precio"]'
     PRODUCTSTOCK='//*[@name="s_cantidad"]'
+    CHECKPRICEXPATH='//*[@name="p_mostrar_precio"]'
+    SUBMITBUTTON='//*[@id="root"]/div/main/div[2]/div/div[6]/div/div/div/div[1]/button'
     
 
    
@@ -37,6 +43,7 @@ def editProductByIdx(i_prod,Values):
         product_name = driver.find_element(By.XPATH, PRODUCTNAMEXPATH).get_attribute('value')
         product_price = driver.find_element(By.XPATH, PRODUCTPRICEXPATH).get_attribute('value')
         product_stock = driver.find_element(By.XPATH, PRODUCTSTOCK).get_attribute('value')
+        product_check_price = driver.find_element(By.XPATH, CHECKPRICEXPATH).get_attribute('value')
         # getNewValues(product_name)
             
         print(product_name)
@@ -44,11 +51,22 @@ def editProductByIdx(i_prod,Values):
             if value[0]==product_name:
                 if product_price!= value[1] or product_stock!= value[2]:
                     print(f'Actualizar precios de {product_name}')
+                    if product_check_price==0:
+                        driver.find_element(By.XPATH, CHECKPRICEXPATH).click()
+                    fillInput(PRODUCTPRICEXPATH,value[1])
+                    fillInput(PRODUCTSTOCK,int(value[2]))
+                    driver.find_element(By.XPATH, SUBMITBUTTON).click()
+                    time.sleep(1)
+
+
+                    
+
+                    
         i_prod+=1
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
         return i_prod
-    except:
+    except IndexError:
         print('Hubo un error')
         return 0
 
@@ -68,16 +86,22 @@ def editProduct(prod,Values):
     	
     print(product_name)
 
-            
+def expandProductsList():
+    see_more_products=True
+    new_click=0
+    while see_more_products:
+        try:
+            see_more_buton= driver.find_element(By.XPATH,'//*[@id="root"]/div/main/div[6]/div/button')
+            see_more_buton.click()
+            time.sleep(2)
+            new_click+=1
+            if new_click==1:
+                print('Aguarde mientras se expande la lisa de productos')
+        except:
+            see_more_products=False
 
-USERXPATH='//*[@id="t_email"]'
-PASSXPATH='//*[@id="t_clave"]'
-BUTTONXPATH='//*[@id="root"]/div/div/div[1]/div/form/div/div[1]/div/div/div/button'
 
-LISTXPATH='//*[@id="root"]/div/main/div[5]/div'
-EDIT_PRODUCTFULLXPATH='/html/body/div[1]/div/main/div[5]/div/div[1]/div/div/div/div/div[2]/div/div[4]/a'
-                   #  '/html/body/div[1]/div/main/div[5]/div/div[1]/div/div/div/div/div[2]/div/div[4]/a'
-                   #  '/html/body/div[1]/div/main/div[5]/div/div[2]/div/div/div/div/div[2]/div/div[4]/a'
+
 
 
 
@@ -85,7 +109,10 @@ Xpath='/html/body/div[4]/div/nav[1]/ul/li[2]/ul/li[4]/a'
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
-driver = webdriver.Chrome('./chromedriver.exe',chrome_options=options)
+try:
+    driver = webdriver.Chrome('./chromedriver.exe',chrome_options=options)
+except:
+    driver = webdriver.Chrome('./chromedriver',chrome_options=options)
 driver.get("https://panel.empretienda.com/login")
 assert "Empretienda" in driver.title
 
@@ -100,17 +127,8 @@ time.sleep(2)
 
 SHEETSVALUES=getNewValues()
 
-see_more_products=True
-new_click=0
-while see_more_products:
-    try:
-        see_more_buton= driver.find_element(By.XPATH,'//*[@id="root"]/div/main/div[6]/div/button')
-        see_more_buton.click()
-        time.sleep(2)
-        new_click+=1
-        print(new_click)
-    except:
-        see_more_products=False
+expandProductsList()
+
 
 
 i_prod=1
---
+++ xpath.py
@@ -0,0 +1,8 @@
+USERXPATH='//*[@id="t_email"]'
+PASSXPATH='//*[@id="t_clave"]'
+BUTTONXPATH='//*[@id="root"]/div/div/div[1]/div/form/div/div[1]/div/div/div/button'
+
+LISTXPATH='//*[@id="root"]/div/main/div[5]/div'
+EDIT_PRODUCTFULLXPATH='/html/body/div[1]/div/main/div[5]/div/div[1]/div/div/div/div/div[2]/div/div[4]/a'
+                   #  '/html/body/div[1]/div/main/div[5]/div/div[1]/div/div/div/div/div[2]/div/div[4]/a'
+                   #  '/html/body/div[1]/div/main/div[5]/div/div[2]/div/div/div/div/div[2]/div/div[4]/a'
